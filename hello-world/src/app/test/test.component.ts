5b924e24cb145fa4ac3cac435f1f48b91848ee05
Darshan Bangoria <darshan@promact.com>
2018-01-08 12:48:32 +0530

    Interpolation, Property Binding, Single and Multiple Class Binding

--- hello-world/src/app/test/test.component.ts
+++ hello-world/src/app/test/test.component.ts
@@ -2,14 +2,40 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.css']
+  template: `<h2>Wecome {{uname}}</h2>
+            <h2>{{2+3}}</h2>
+            <h2>{{"Welcome "+uname}}</h2>
+            <h2>{{uname.toUpperCase()}}</h2>
+            <h2>{{uname.length}}</h2>
+            <h2>{{greet()}}</h2>
+            <h2 class="text-success">Welcome Dexter</h2>
+            <h2 [class]="successClass">Welcome Dexter</h2>
+            <h2 [class.text-danger]="hasError">Welcome Dexter</h2>
+            <h2 [ngClass]="msgObject">Welcome Dexter</h2>`,
+  styles: [`
+      .text-success{color:green;}
+      .text-danger{color:red;}
+      .text-special{font-style:italic;}
+  `]
 })
 export class TestComponent implements OnInit {
+
+  public uname="Darshan";
+  public successClass="text-success";
+  public hasError=false;
+  public isSpecial=true;
+  public msgObject = {
+      "text-success": !this.hasError,
+      "text-danger": this.hasError,
+      "text-special": this.isSpecial
+  }
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  greet(){
+    return "Hello "+this.uname;
+  }
 }
