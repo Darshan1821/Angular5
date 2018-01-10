ea58a45fa820c2ccdcfce042260060a19281facd
Darshan Bangoria <darshanbangoria.2@gmail.com>
2018-01-10 11:04:34 +0530

    Style Binding and added comments

--- hello-world/src/app/test/test.component.ts
+++ hello-world/src/app/test/test.component.ts
@@ -2,16 +2,40 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  template: `<h2>Wecome {{uname}}</h2>
-            <h2>{{2+3}}</h2>
-            <h2>{{"Welcome "+uname}}</h2>
-            <h2>{{uname.toUpperCase()}}</h2>
-            <h2>{{uname.length}}</h2>
-            <h2>{{greet()}}</h2>
-            <h2 class="text-success">Welcome Dexter</h2>
-            <h2 [class]="successClass">Welcome Dexter</h2>
-            <h2 [class.text-danger]="hasError">Welcome Dexter</h2>
-            <h2 [ngClass]="msgObject">Welcome Dexter</h2>`,
+  template: `
+            <!--Interpolation-->
+            <h3>Interpolation:</h3>
+            <h4>Wecome {{uname}}</h4>
+            <h4>{{2+3}}</h4>
+            <h4>{{"Welcome "+uname}}</h4>
+            <h4>{{uname.toUpperCase()}}</h4>
+            <h4>{{uname.length}}</h4>
+            <h4>{{greet()}}</h4>
+
+            <br><br>
+            <!--Property Binding-->
+            <h3>Property Binding:</h3>
+            <input [id]="myId" text="Darshan" value="Darshan"/>
+            
+            <br><br>
+            <!--Single Class Binding-->
+            <h3>Single Class Binding:</h3>
+            <h4 class="text-success">Welcome Dexter</h4>
+            <h4 [class]="successClass">Welcome Dexter</h4>
+            <h4 [class.text-danger]="hasError">Welcome Dexter</h4>
+            
+            <br><br>
+            <!--Multiple Class Binding-->
+            <h3>Multiple Class Binding:</h3>
+            <h4 [ngClass]="msgObject">Welcome Dexter</h4>
+            
+            <br><br>
+            <!--Style Binding-->
+            <h3>Style Binding:</h3>
+            <h4 [style.color]="'orange'">Welcome Dexter</h4>
+            <h4 [style.color]="myColor">Welcome Dexter</h4>
+            <h4 [style.color]="hasError ? 'red' : 'green'">Welcome Dexter</h4>
+            <h4 [ngStyle]="myStyles">Welcome Dexter</h4>`,
   styles: [`
       .text-success{color:green;}
       .text-danger{color:red;}
@@ -21,13 +45,19 @@ import { Component, OnInit } from '@angular/core';
 export class TestComponent implements OnInit {
 
   public uname="Darshan";
+  public myId="username";
   public successClass="text-success";
   public hasError=false;
   public isSpecial=true;
+  public myColor="blue";
   public msgObject = {
       "text-success": !this.hasError,
       "text-danger": this.hasError,
       "text-special": this.isSpecial
+  }
+  public myStyles = {
+    color:"purple",
+    fontStyle:"italic"
   }
 
   constructor() { }
